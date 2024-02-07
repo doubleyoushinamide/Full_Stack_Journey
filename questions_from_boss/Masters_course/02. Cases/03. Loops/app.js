ca30c19e44e55dd3b155e3c5110747a117f67128
doubleyoushinamide <olusholasalau@gmail.com>
2024-02-07 23:59:15 +0100

    for loops in JavaScript

---
+++ questions_from_boss/Masters_course/02. Cases/03. Loops/app.js
@@ -0,0 +1,8 @@
+// //for (declareVariable; condition; HowToIncrement){
+// }
+
+// Printing my name by 1000 on the console
+
+for (let myName = 0; myName <= 1000; myName++) {
+  console.log('Shina', myName);
+}
