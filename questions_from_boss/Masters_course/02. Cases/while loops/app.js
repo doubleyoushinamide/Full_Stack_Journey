9eb2d737335fbd7b431c45e4ef90d4d76f495795
doubleyoushinamide <olusholasalau@gmail.com>
2024-02-08 20:19:46 +0100

    pushing the while loops

---
+++ questions_from_boss/Masters_course/02. Cases/while loops/app.js
@@ -0,0 +1,25 @@
+// while loops pseudocode
+// format
+
+/**
+ * declare variable here;
+ *
+ * while (condition) {
+ *      code blocks here
+ *      increment++ or decrement--
+ * }
+ */
+
+let start = 1;
+while (start <= 6) {
+  console.log('Welcome to the family', start);
+  start++;
+}
+
+// Challenge here
+
+let name2 = 10;
+while (name2 <= 100) {
+  console.log('Shina Salau', name2);
+  name2++;
+}
