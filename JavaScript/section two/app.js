340e9f4b265c613259071f8d5f30c3ce659bb954
doubleyoushinamide <olusholasalau@gmail.com>
2024-12-22 21:50:35 +0100

    challenge one done

---
+++ JavaScript/section two/app.js
@@ -0,0 +1,12 @@
+let x; //defining a usabe variable
+
+//creating an array
+const arr = [1, 2, 3, 4, 5];
+
+//manipulating the array
+x = arr.sort();
+x = arr.push(6);
+x = arr.reverse();
+console.log(x);
+
+// 2. Challenge two.
